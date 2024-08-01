39a03c04e301de9c9bae28e1cef21e53d738f3de
ardaozkanli <ardaozkanli@hotmail.com>
2024-08-01 10:32:48 +0300

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
+import { useState } from "react"
 
 function App() {
-
-
+  const [currentNumber,setCurrentNumber] = useState(0);
+  function handleIncrement (){
+    setCurrentNumber((prev) => prev + 1);
+  }
+  function handleDecrement (){
+     setCurrentNumber((prev) => prev -1 );
+  }
   return (
     <div>
-      saaasasasa
+      <h1>{currentNumber}</h1>
+      <button onClick={handleIncrement}>Değeri Arttır</button>
+      <button onClick={handleDecrement}>Değeri Azalt</button>
     </div>
-  )
+  );
 }
 
 export default App
